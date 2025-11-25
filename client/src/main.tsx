8039677e9b72f2bf8d9038c9fe49eadec0ebdaef
Fatemeh Eshghi <157709672+Fatemeh-Eshghi@users.noreply.github.com>
2025-11-25 12:08:52 +0200

    merge main

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,8 +4,10 @@ import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router.tsx";
 import { GoogleOAuthProvider } from "@react-oauth/google";
+import "/styles/global.css"
 
 const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
+
 createRoot(document.getElementById("root")!).render(
 <GoogleOAuthProvider clientId={clientId}>
 
