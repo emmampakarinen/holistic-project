cd99947bdef76d9080cf6c84bae710ff0c5520de
Fatemeh Eshghi <157709672+Fatemeh-Eshghi@users.noreply.github.com>
2025-11-25 12:50:54 +0200

    Fix merge conflict in main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,18 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-<<<<<<< HEAD
+
 //import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router.tsx";
 import "./styles/global.css"
-=======
+
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router.tsx";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 
-
 const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
->>>>>>> feature-24-clean
 
 createRoot(document.getElementById("root")!).render(
 <GoogleOAuthProvider clientId={clientId}>
