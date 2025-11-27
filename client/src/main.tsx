1bc99479330883627cced3dc1d081b95b7fecaf3
Fatemeh Eshghi <157709672+Fatemeh-Eshghi@users.noreply.github.com>
2025-11-27 10:54:11 +0200

    edit man

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,16 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-
-//import "./index.css";
-import { RouterProvider } from "react-router-dom";
-import { router } from "./router.tsx";
-import "./styles/global.css"
-
-import { RouterProvider } from "react-router-dom";
-import { router } from "./router.tsx";
-import { GoogleOAuthProvider } from "@react-oauth/google";
 import "./styles/global.css";
-//import "./index.css";
+
+import { RouterProvider } from "react-router-dom";
+import { router } from "./router";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
 
