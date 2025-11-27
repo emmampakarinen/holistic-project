c0ea99f0b1c8f7fa1d3c29b30a75d6bc749dea4a
Tahmid Nur Refat <144625678+TahmidNurRefat@users.noreply.github.com>
2025-11-27 09:10:33 +0000

    update

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -7,15 +7,5 @@ export default defineConfig({
   server: {
     host: true,
     port: 3000,
-    strictPort: true,
-    hmr: { host: "localhost", clientPort: 3000 },
-    watch: { usePolling: true, interval: 150 },
-    proxy: {
-      "/api": {
-        target: "http://server:5000",
-        changeOrigin: true,
-      },
-    },
   },
-  preview: { port: 3000, host: true },
 });
